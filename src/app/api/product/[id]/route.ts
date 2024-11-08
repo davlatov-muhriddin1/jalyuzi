2672e51182a3d659634335e34cdc9aa112eb48f2
Davlatov Muhriddin <162937360+davlatov-muhriddin1@users.noreply.github.com>
2024-11-08 05:34:59 +0500

    change api

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -2,11 +2,14 @@ import Product from "@/lib/models/Product";
 import { dbConnect } from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, route: { params: { id: string } }) {
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    dbConnect();
+    await dbConnect();
 
-    const singleProduct = await Product.findById(route.params.id);
+    const singleProduct = await Product.findById(params.id);
 
     if (!singleProduct) {
       return NextResponse.json({ message: "Mahsulot topilmadi" });
@@ -17,18 +20,6 @@ export async function GET(req: Request, route: { params: { id: string } }) {
       product: singleProduct,
     });
   } catch (error) {
-    NextResponse.json({ message: "Server Error", error });
-  }
-}
-
-export async function DELETE(req: Request, route: { params: { id: string } }) {
-  try {
-    await dbConnect();
-
-    await Product.findByIdAndDelete(route.params.id);
-
-    return NextResponse.json({ message: "Mahsulot ochirildi" });
-  } catch (error) {
-    NextResponse.json({ message: "Server Error", error });
+    return NextResponse.json({ message: "Server Error", error });
   }
 }
