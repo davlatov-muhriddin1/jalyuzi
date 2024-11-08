80c6f94a47feb0b4fc1fe75de9d86cfe7baea9a9
Davlatov Muhriddin <162937360+davlatov-muhriddin1@users.noreply.github.com>
2024-11-08 05:25:14 +0500

    add eslink settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["salmon-sophisticated-alligator-592.mypinata.cloud"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
